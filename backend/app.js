2b2cffa4ed431e2e47b3423987d02cb29a1e5ddc
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2021-11-03 09:47:09 +0300

    updates

--- backend/app.js
+++ backend/app.js
@@ -28,11 +28,14 @@ app.use(cookieParser());
 app.use('/customer', indexRouter);
 app.use('/vendor', usersRouter);
 app.use('/admin', adminRouter);
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../frontend/build'));
 
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
